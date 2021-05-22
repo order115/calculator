0021cebf79f5de89688c9de205a31ad766a4cc9b
Viktor Nogin <orderboot115@gmail.com>
2021-05-22 18:14:06 +0300

    feat: simple operation (2+2=4) is working

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,61 +6,40 @@ const Calculator = () => {
   const [display, setDisplay] = useState("0");
   const [store, setStore] = useState("0");
   const [value, setValue] = useState("0");
-  const [operation, setOperation] = useState();
-  const [clearTitle, setClearTitle] = useState("AC");
+  const [operation, setOperation] = useState("");
 
   useEffect(() => {
-    console.log(
-      "Display:",
-      display,
-      "Store:",
-      store,
-      "Value:",
-      value,
-      "Operation:",
-      operation
-    );
-  }, [display, store, , operation]);
-
-  const math = (x, oper, y) => {
-    return eval(myArray.join(" "));
-  };
+    console.log(`${store} ${operation} ${value} = ${display}`);
+  }, [display, store, value, operation]);
 
   const clearStates = () => {
     setDisplay("0");
     setValue("0");
     setStore("0");
+    setOperation("");
   };
 
-  const handleNumButton = (num) => {
-    const enterValue = (value + num).replace(/^0+/, "");
-
-    if (+enterValue) {
-      setDisplay(enterValue);
-      setValue(enterValue);
-    }
-
-    // const newValue = String(
-    //   +value === 0 ? (+num === 0 ? 0 : num) : value + num
-    // );
-    // if (!+value) {
-    //   setValue(newValue);
-    //   setDisplay(newValue);
-    // }
-    // // setDisplay(newValue);
-    // // setValue(newValue);
-    // setClearTitle("C");
+  const handleEnter = (enter) => {
+    const newValue = (value + enter).replace(/^0+/, "");
+    setValue(newValue);
+    setDisplay(newValue);
   };
 
-  const handleOperationButton = (operation) => {
+  const handleOperation = (operation) => {
     setOperation(operation);
-    if (+store && +value) {
-      setStore(eval(store + operation + value));
-      setDisplay(eval(store + operation + value));
+
+    if (+store) {
+      setStore(store);
     } else {
       setStore(value);
-      setDisplay(value);
+      setValue("0");
     }
+  };
+
+  const handleEqual = () => {
+    const equal = eval(store + operation + value).toString();
+    setDisplay(equal);
+    setStore(equal);
   };
 
   return (
@@ -73,44 +52,44 @@ const Calculator = () => {
             clearStates();
           }}
         >
-          {clearTitle}
+          AC
         </Button>
-        <Button color="secondary" onClick={() => handleOperationButton("+")}>
+        <Button color="secondary" onClick={() => handleOperation("+")}>
           +/-
         </Button>
-        <Button color="secondary" onClick={() => handleOperationButton("%")}>
+        <Button color="secondary" onClick={() => handleOperation("%")}>
           %
         </Button>
-        <Button color="primary" onClick={() => handleOperationButton("/")}>
+        <Button color="primary" onClick={() => handleOperation("/")}>
           /
         </Button>
-        <Button onClick={() => handleNumButton("6")}>6</Button>
-        <Button onClick={() => handleNumButton("8")}>8</Button>
-        <Button onClick={() => handleNumButton("9")}>9</Button>
-        <Button color="primary" onClick={() => handleOperationButton("*")}>
+        <Button onClick={() => handleEnter("6")}>6</Button>
+        <Button onClick={() => handleEnter("8")}>8</Button>
+        <Button onClick={() => handleEnter("9")}>9</Button>
+        <Button color="primary" onClick={() => handleOperation("*")}>
           *
         </Button>
-        <Button onClick={() => handleNumButton("4")}>4</Button>
-        <Button onClick={() => handleNumButton("5")}>5</Button>
-        <Button onClick={() => handleNumButton("6")}>6</Button>
-        <Button color="primary" onClick={() => handleOperationButton("-")}>
+        <Button onClick={() => handleEnter("4")}>4</Button>
+        <Button onClick={() => handleEnter("5")}>5</Button>
+        <Button onClick={() => handleEnter("6")}>6</Button>
+        <Button color="primary" onClick={() => handleOperation("-")}>
           -
         </Button>
-        <Button onClick={() => handleNumButton("1")}>1</Button>
-        <Button onClick={() => handleNumButton("2")}>2</Button>
-        <Button onClick={() => handleNumButton("3")}>3</Button>
-        <Button color="primary" onClick={() => handleOperationButton("+")}>
+        <Button onClick={() => handleEnter("1")}>1</Button>
+        <Button onClick={() => handleEnter("2")}>2</Button>
+        <Button onClick={() => handleEnter("3")}>3</Button>
+        <Button color="primary" onClick={() => handleOperation("+")}>
           +
         </Button>
         <Button
           size="double"
-          onClick={() => handleNumButton("0")}
+          onClick={() => handleEnter("0")}
           className={styles.double}
         >
           0
         </Button>
-        <Button onClick={() => handleNumButton(".")}>.</Button>
-        <Button color="primary" onClick={() => handleOperationButton("=")}>
+        <Button onClick={() => handleEnter(".")}>.</Button>
+        <Button color="primary" onClick={() => handleEqual()}>
           =
         </Button>
       </div>
